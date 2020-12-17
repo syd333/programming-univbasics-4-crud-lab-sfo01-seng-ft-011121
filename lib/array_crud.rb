caf74a00563f7afb825da848c04aa6d9c433925e
sydney miranda <sydmiranda@icloud.com>
2020-12-17 23:02:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ four_elements = ['Sydney', 333, true, 'Cyntelle']
 end
 
 def add_element_to_end_of_array(array, element)
-
+array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
